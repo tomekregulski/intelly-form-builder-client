59ae3bba83c04ec5af2edee72af076345b8feaf6
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-12-07 11:26:14 -0500

    Add basic form elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Input from './components/Input';
+import Select from './components/Select';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Form Builder</h1>
+      <Input />
+      <Select />
     </div>
   );
 }
---
+++ src/components/Select.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Select = () => {
+  return (
+    <div>
+      <label></label>
+      <select>
+        <option value=''>--Please Select an Option</option>
+      </select>
+    </div>
+  );
+};
+
+export default Select;
